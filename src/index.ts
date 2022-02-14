770787108aacca314045a8feb90a757480502333
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-02-15 00:50:29 +0900

    feat: index에서 Store, App을 불러오는 작업 추가

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import App from './App';
+import Store from './lib/redux/Store';
+import reduce from '@/lib/redux/reducer';
 
-const $app = document.querySelector('#app')! as HTMLElement;
+export interface State {}
 
-new App($app);
+// Store 내부에서 사용 될 State가 결정되면 Generic으로 넘겨줄 것
+export const store = new Store<State>();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const $app = document.querySelector('#app')! as HTMLElement;
+  const app = new App($app);
+  store.setup(app.state, reduce);
+});
