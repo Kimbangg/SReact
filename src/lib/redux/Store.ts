339490a2205858a7978ebe28ea0fe931642ad89c
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-02-15 00:51:17 +0900

    refactor: Store 코드 > 1차 정리(notifySubscribers Error 존재)

--- src/lib/redux/Store.ts
+++ src/lib/redux/Store.ts
@@ -1,33 +1,41 @@
-export default class Store {
-  state;
+import { Action } from '@/types/Store';
+import { BaseComponent } from '@/lib/core/Component';
+
+export interface State {}
+
+export default class Store<S = State> {
+  state: S;
   reduce: Function;
   subscribers: Array<Function>;
 
   constructor() {
-    this.state = {};
+    this.state = {} as S;
     this.reduce = () => {};
     this.subscribers = [];
   }
 
-  public setup(reduce: Function) {
-    reduce ? (this.reduce = reduce) : '';
-    this.state = this.reduce(this.state, {});
+  public setup(state: any, reduce: Function) {
+    this.reduce = reduce ?? function () {};
+    this.state = this.reduce(state, {});
   }
 
   public subscribe(fn: Function) {
+    // subscribe Fn = Component.render.bind(this);
     this.subscribers.push(fn);
   }
 
-  get getState() {
+  public getState() {
     return this.state;
   }
 
-  notifyToSubScribers() {
+  private notifySubScribers() {
     this.subscribers.forEach(fn => fn());
   }
 
-  dispatch(action: String) {
+  // dispatch에게 필요한 action을 넘겨준다.
+  public dispatch(action: Action) {
+    // dispatch는 action과 state를 reducer에게 넘겨준다.
     this.state = this.reduce(this.state, action);
-    this.notifyToSubScribers();
+    this.notifySubScribers();
   }
 }
