357560f6ff591ce261c052c44e2acc6a153ffbf2
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-02-15 14:39:45 +0900

    feat: 예시 코드 업로드

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import App from './App';
 import Store from './lib/redux/Store';
-// import reduce from '@/lib/redux/reducer';
+import reduce from '@/lib/redux/reducer';
 
-export interface State {}
+export interface State {
+  countNumber: number;
+}
 
 // Store 내부에서 사용 될 State가 결정되면 Generic으로 넘겨줄 것
 export const store = new Store<State>();
@@ -10,5 +12,5 @@ export const store = new Store<State>();
 document.addEventListener('DOMContentLoaded', () => {
   const $app = document.querySelector('#app')! as HTMLElement;
   const app = new App($app);
-  // store.setup(app.state, reduce);
+  store.setup(app.state, reduce);
 });
