939f68e38b90a17f01e141a7d93271f313fb44d2
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-03-04 22:40:28 +0900

    refactor: App은 현재 URL에 맞는 컴포넌트를 렌더링 하는 역할만 수행하도록 변경

--- src/App.ts
+++ src/App.ts
@@ -1,33 +1,49 @@
 import '@/styles/index.scss';
+import BrowserRouter from './lib/Router';
 // import { store } from '.';
 // import { increaseNumber } from './lib/redux/action';
-import { BaseComponent } from './lib/core/Component';
 
-export default class App extends BaseComponent<HTMLElement> {
-  // $minusBtn: HTMLButtonElement;
-  // $plusBtn: HTMLButtonElement;
-  // setup() {
-  //   this.state = 1;
-  //   store.subscribe(this.render.bind(this));
-  // }
-  // template() {
-  //   return `
-  //     <div>
-  //       <p>현재 state 값은 ${store.getState().countNumber ?? this.state}</p>
-  //     <div>
-  //     <div>
-  //       <button id="plus-btn">플러스</button>
-  //       <button id="minus-btn">마이너스</button>
-  //     </div>
-  //   `;
-  // }
-  // selectDom() {
-  //   this.$plusBtn = document.querySelector('#plus-btn')! as HTMLButtonElement;
-  //   this.$minusBtn = document.querySelector('#minus-btn')! as HTMLButtonElement;
-  // }
-  // setEvent() {
-  //   this.$plusBtn.addEventListener('click', () => {
-  //     store.dispatch(increaseNumber());
-  //   });
-  // }
+interface State {}
+
+export default class App<State> {
+  $target: HTMLElement;
+  browserRouter: BrowserRouter;
+
+  constructor($target: HTMLElement) {
+    this.$target = $target;
+
+    this.browserRouter = new BrowserRouter('');
+    this.switchRender();
+    this.browserRouter.setup(this.switchRender.bind(this));
+  }
+  switchRender() {
+    const { pathname } = window.location;
+    const { routeChange } = this.browserRouter;
+
+    const pathURL =
+      pathname.indexOf('/web/products') === 0 ? '/web/products' : pathname;
+
+    // switch (pathURL) {
+    //   case PRODUCT_LIST_PAGE:
+    //     new ProductListPage(this.$target, {
+    //       routeChange,
+    //     });
+    //     break;
+
+    //   case PRODUCT_LIST_PAGE:
+    //     const [, , , productId] = pathname.split('/');
+    //     new ProductDetailPage(this.$target, {
+    //       productId,
+    //       routeChange,
+    //     });
+    //     break;
+
+    //   case CART_PAGE:
+    //     new Cart(this.$target, {
+    //       routeChange,
+    //     });
+    //     break;
+    //  }
+    // }
+  }
 }
