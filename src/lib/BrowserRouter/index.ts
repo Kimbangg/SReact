dfbe663f8fdc0410fb3af5db70797a7da224ef78
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-03-31 15:37:07 +0900

    refactor: 라우터 초기 설정 방법 변경

--- src/lib/BrowserRouter/index.ts
+++ src/lib/BrowserRouter/index.ts
@@ -3,28 +3,23 @@ const POP_STATE = 'popstate';
 
 // Browser 라우터는 URL을 이동 시킨 뒤 App에게 뷰(=Component)를 바꾸라는 요청을 하는 객체이다.
 export default class BrowserRouter {
-  switchRouter: Function | null;
+  switchRender: Function;
 
-  constructor(initialURL: string) {
-    this.switchRouter = null;
+  constructor(switchRender: Function) {
+    this.switchRender = this.switchRender;
 
     this.setEvent();
-    this.routeChange(initialURL);
-  }
-
-  setup(switchRouter: Function) {
-    this.switchRouter = switchRouter;
   }
 
   setEvent() {
     // URL의 변화가 생겼을 때, App에게 알리는 역할을 수행
     window.addEventListener(ROUTE_CHANGE, () => {
-      this.switchRouter && this.switchRouter();
+      this.switchRender && this.switchRender();
     });
 
     // URL의 이동을 확인했을 때, App에게 알리는 역할을 수행
     window.addEventListener(POP_STATE, () => {
-      this.switchRouter && this.switchRouter();
+      this.switchRender && this.switchRender();
     });
   }
 
